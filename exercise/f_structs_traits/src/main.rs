639d6bd8c2b9e65c8958c0c6eea7bcb73c596ebb
Adam Zawierucha <zawiedev@gmail.com>
2022-06-12 14:39:25 -0700

    excercise f

--- exercise/f_structs_traits/src/main.rs
+++ exercise/f_structs_traits/src/main.rs
@@ -5,7 +5,9 @@
 // `cargo run` without any errors.
 //
 //  trait Bite...
-
+trait Bite {
+    fn bite(self: &mut Self);
+}
 
 // 2. Now create a struct named Grapes with a field that tracks how many grapes are left.  If you
 // need a hint, look at how it was done for Carrot at the bottom of this file (you should probably
@@ -13,13 +15,20 @@
 //
 // #[derive(Debug)] // include this line right before your struct definition
 // struct Grapes...
-
+#[derive(Debug)] // This enables using the debugging format string "{:?}"
+struct Grapes {
+    count: i32,
+}
 
 // 3. Implement Bite for Grapes.  When you bite a Grapes, subtract 1 from how many grapes are left.
 // If you need a hint, look at how it was done for Carrot at the bottom of this file.
 //
 // impl Bite for...
-
+impl Bite for Grapes {
+    fn bite(self: &mut Self) {
+        self.count -= 1;
+    }
+}
 
 fn main() {
     // Once you finish #1 above, this part should work.
@@ -30,9 +39,9 @@ fn main() {
     // 4. Uncomment and adjust the code below to match how you defined your
     // Grapes struct.
     //
-    //let mut grapes = Grapes { amount_left: 100 };
-    //grapes.bite();
-    //println!("Eat a grape: {:?}", grapes);
+    let mut grapes = Grapes { count: 100 };
+    grapes.bite();
+    println!("Eat a grape: {:?}", grapes);
 
     // Challenge: Uncomment the code below. Create a generic `bunny_nibbles`
     // function that:
@@ -41,8 +50,14 @@ fn main() {
     // Hint: Define the generic type between the function name and open paren:
     //       fn function_name<T: Bite>(...)
     //
-    //bunny_nibbles(&mut carrot);
-    //println!("Bunny nibbles for awhile: {:?}", carrot);
+    bunny_nibbles(&mut carrot);
+    println!("Bunny nibbles for awhile: {:?}", carrot);
+}
+
+fn bunny_nibbles<T: Bite>(food: &mut T) {
+    for _ in 0..3 {
+        food.bite();
+    }
 }
 
 #[derive(Debug)] // This enables using the debugging format string "{:?}"
