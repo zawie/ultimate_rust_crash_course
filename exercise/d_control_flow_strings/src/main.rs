b8d0a56eecf99f46245c804bac73e4bc58abd1af
Adam Zawierucha <zawiedev@gmail.com>
2022-06-12 10:12:58 -0700

    d modifications

--- exercise/d_control_flow_strings/src/main.rs
+++ exercise/d_control_flow_strings/src/main.rs
@@ -38,7 +38,7 @@ fn sum() {
     // 2. Use a "for loop" to iterate through integers from 7 to 23 *inclusive* using a range
     // and add them all together (increment the `sum` variable).  Hint: You should get 255
     // Run it with `cargo run sum`
-    for i in 7..24 {
+    for i in 7..=23 {
         sum += i;
     }
 
